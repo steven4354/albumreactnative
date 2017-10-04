306932b4dab8f5aaa35f1abf9f7a2362e3b438a5
steven4354 <steven4354@gmail.com>
2017-10-04 00:45:07 -0700

    initial index.js setup

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+//import { AppRegistry } from 'react-native';
+//import App from './App';
 
+//AppRegistry.registerComponent('albums', () => App);
+//^^old original code
+
+//Import a library to help create a Component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+//Create a Componen
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+//Add the Component to the device
 AppRegistry.registerComponent('albums', () => App);
